3de9774e3292eab31353484872e857a6e82da894
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2016-12-20 09:25:30 +0700

    Refactor specs

--- installer/lib/installer.rb
+++ installer/lib/installer.rb
@@ -3,3 +3,62 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path("../../Gemfile", Pathname.new(__FILE_
 
 require 'rubygems'
 require 'bundler/setup'
+
+class Installer
+  INSTALLER_CMD = 'install.sh'
+  INSTALLER_PATH = File.expand_path(File.dirname(__FILE__)) + "/../bin/#{INSTALLER_CMD}"
+
+  attr_reader :env, :args, :prompts_with_values, :stdout, :stderr, :ret_value
+
+  def initialize(env: {}, args: '', prompts_with_values: {})
+    @env, @args, @prompts_with_values = env, args, prompts_with_values
+  end
+
+  def call
+    Open3.popen3(stringified_env, "#{INSTALLER_PATH} #{args}") do |stdin, stdout, stderr, wait_thr|
+      stdout.sync = true
+      stdin.sync = true
+
+      @stdout = emulate_interactive_io(stdin, stdout)
+      @stderr = Thread.new { stderr.read }.value
+      @ret_value =  wait_thr.value
+    end
+  end
+
+  private
+
+  def stringified_env
+    env.map { |k, v| [k.to_s, v.to_s] }.to_h
+  end
+
+  def emulate_interactive_io(stdin, stdout)
+    out = ''
+    reader_thread = Thread.new {
+      begin
+        stdout_chunk = read_stream(stdout)
+        break if stdout_chunk.nil?
+
+        out << stdout_chunk
+        prompt = stdout_chunk.split("\n").last
+        stdin.puts(prompts_with_values[prompt])
+      end while true
+    }
+    reader_thread.join
+    out
+  end
+
+  def read_stream(stdout)
+    buffer = ''
+
+    begin
+      char = stdout.getc
+      return if char.nil?
+
+      buffer << char
+    end while char != '>'
+
+    buffer << stdout.getc
+    buffer
+  end
+
+end
--- installer/spec/installer/installer_spec.rb
+++ installer/spec/installer/installer_spec.rb
@@ -3,9 +3,6 @@ require 'spec_helper'
 require 'open3'
 require 'tmpdir'
 
-INSTALLER_CMD = 'install.sh'
-INSTALLER_PATH = File.expand_path(File.dirname(__FILE__)) + "/../../bin/#{INSTALLER_CMD}"
-
 describe 'installer.sh' do
   around(:example) do |example|
     Dir.mktmpdir do |dir|
@@ -30,81 +27,35 @@ describe 'installer.sh' do
 
   let(:stdin_data) { en_data }
 
-  def read_stream(stdout)
-    buffer = ''
-
-    begin
-      char = stdout.getc
-
-      return if char.nil?
-
-      buffer << char
-    end while char != '>'
-
-    buffer << stdout.getc
-
-    buffer
-  end
+  let(:installer) { Installer.new(env: env, args: args, prompts_with_values: stdin_data) }
 
   def invoke_installer_sh
-    env_vars = env.map { |k, v| [k.to_s, v] }.to_h
-    Open3.popen3(env_vars, "#{INSTALLER_PATH} #{args}") do |stdin, stdout, stderr, wait_thr|
-      stdout.sync = true
-      stdin.sync = true
-
-      out = ''
-
-      out_thr = Thread.new {
-        begin
-          out_chunk = read_stream(stdout)
-
-          break if out_chunk.nil?
-
-          out << out_chunk
-
-          prompt = out_chunk.split("\n").last
-
-          stdin.puts(stdin_data[prompt])
-        end while true
-      }
-      out_thr.join
-
-      err = Thread.new { stderr.read }.value
-
-      [out, err, wait_thr.value]
-    end
-  end
-
-  def get_stdout(invoke_installer_sh_result)
-    invoke_installer_sh_result[0]
-  end
-
-  def get_stderr(invoke_installer_sh_result)
-    invoke_installer_sh_result[1]
-  end
-
-  def get_result(invoke_installer_sh_result)
-    invoke_installer_sh_result[2]
+    installer.call
   end
 
   shared_examples_for 'should not print anything to stderr' do
-    specify { expect(get_stderr(invoke_installer_sh)).to be_empty }
+    before { installer.call }
+    specify { expect(installer.stderr).to be_empty }
   end
 
   shared_examples_for 'should print to stdout' do |expected_text|
-    specify { expect(get_stdout(invoke_installer_sh)).to match(expected_text) }
+    before { installer.call }
+    specify { expect(installer.stdout).to match(expected_text) }
   end
 
   shared_examples_for 'should print to stderr' do |expected_text|
-    specify { expect(get_stderr(invoke_installer_sh)).to match(expected_text) }
+    before { installer.call }
+    specify { expect(installer.stderr).to match(expected_text) }
   end
 
   shared_examples_for 'should exit with error' do
-    specify { expect(get_result(invoke_installer_sh)).not_to be_success }
+    before { installer.call }
+    specify { expect(installer.ret_value).not_to be_success }
   end
 
   shared_examples_for 'should exit without errors' do
-    specify { expect(get_result(invoke_installer_sh)).to be_success }
+    before { installer.call }
+    specify { expect(installer.ret_value).to be_success }
   end
 
   describe 'invoked' do
@@ -112,7 +63,7 @@ describe 'installer.sh' do
       let(:args) { '-x' }
 
       it_behaves_like 'should exit with error'
-      it_behaves_like 'should print to stderr', "Usage: #{INSTALLER_CMD}"
+      it_behaves_like 'should print to stderr', "Usage: #{Installer::INSTALLER_CMD}"
     end
 
     context 'with `-v` `-p` args' do
