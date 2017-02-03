ccba288d1a913c0833885141981831e1a248c262
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2017-02-03 23:31:16 +0700

    Fix incorrect command escaping

--- scripts/enable-ssl.sh
+++ scripts/enable-ssl.sh
@@ -798,7 +798,7 @@ add_renewal_job(){
     local hour="$(date +'%H')"
     local minute="$(date +'%M')"
     local renew_job="${hour} ${minute} * * * ${renew_cmd}"
-    local schedule_renewal_job_cmd="(crontab -l -u nginx; echo '${renew_job}') | crontab -u nginx"
+    local schedule_renewal_job_cmd="(crontab -l -u nginx; echo \"${renew_job}\") | crontab -u nginx"
     run_command "${schedule_renewal_job_cmd}" "$(translate 'messages.schedule_renewal_job')" "hide_output"
   fi
 }
