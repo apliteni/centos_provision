983a43d5589a4461a759644b7e9370578976fc57
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2017-03-10 09:25:54 +0700

    Generate site vhost

--- scripts/spec/scripts/shared_contexts/run_in_docker.rb
+++ scripts/spec/scripts/shared_contexts/run_in_docker.rb
@@ -1,7 +1,7 @@
 require 'shell'
 
 RSpec.shared_context 'run in docker', run_in_docker: :yes do
-  before(:all) { `docker rm keitaro_scripts_test &>/dev/null` }
+  before(:all) { `docker rm -f keitaro_scripts_test &>/dev/null` }
 
   let(:docker_image) { 'centos' }
 end
