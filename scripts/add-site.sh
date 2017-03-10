86a9469f9e752fea2a4021e22ad69f44632a0f29
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2017-03-10 18:36:30 +0700

    Show default site root

--- scripts/add-site.sh
+++ scripts/add-site.sh
@@ -752,6 +752,7 @@ get_user_vars(){
   debug 'Read vars from user input'
   hack_stdin_if_pipe_mode
   get_user_var 'site_domains' 'validate_presence'
+  VARS['site_root']="/var/www/$(first_domain)"
   get_user_var 'site_root' 'validate_presence'
 }
 
