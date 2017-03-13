e8c1406fd3f7f40572eed5c644ccaa4d717513b4
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2017-03-13 10:55:56 +0700

    Strict domain validation

--- scripts/install.sh
+++ scripts/install.sh
@@ -519,6 +519,16 @@ validate_presence(){
 }
 
 
+SUBDOMAIN_REGEXP="[[:alnum:]-]+"
+DOMAIN_REGEXP="(${SUBDOMAIN_REGEXP}\.)+[[:alpha:]]${SUBDOMAIN_REGEXP}"
+DOMAIN_LIST_REGEXP="${DOMAIN_REGEXP}(,${DOMAIN_REGEXP})*"
+
+validate_domains_list(){
+  local value="${1}"
+  [[ "$value" =~ ^(${DOMAIN_LIST_REGEXP})$ ]]
+}
+
+
 
 is_no(){
   local answer="${1}"
