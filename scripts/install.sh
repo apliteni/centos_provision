e1f732e576e40d116dc97e47f22fc0c3805368ce
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2017-04-14 22:17:03 +0700

    Fix incorrect ifs

--- scripts/install.sh
+++ scripts/install.sh
@@ -132,7 +132,7 @@ assert_caller_root(){
   if isset "$SKIP_CHECKS"; then
     debug "SKIP: actual checking of current user"
   else
-    if [[ "$EUID" = "$ROOT_UID" ]]; then
+    if [[ "$EUID" == "$ROOT_UID" ]]; then
       debug 'OK: current user is root'
     else
       debug 'NOK: current user is not root'
