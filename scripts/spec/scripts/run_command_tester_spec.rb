f7e17dc8e0a72e9167b306a55bf2aa54a847aded
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2017-04-20 13:49:12 +0700

    Move print.sh on aprropriate place

--- scripts/spec/scripts/run_command_tester_spec.rb
+++ scripts/spec/scripts/run_command_tester_spec.rb
@@ -10,40 +10,46 @@ RSpec.describe 'test-run-command.sh' do
   let(:script_name) { 'test-run-command.sh' }
   let(:print_sh_counter) { 30 }
   let(:run_command_args) {}
-  let(:args) { "'#{ROOT_PATH}/bin/print.sh #{print_sh_counter} #{print_sh_exit_code}' #{run_command_args}" }
+  let(:helper_scripts_path) { "#{File.dirname(File.expand_path(__FILE__, '../'))}/run_command_tester" }
 
-  shared_examples_for 'prints lines to' do |destination|
-    it_behaves_like 'should print to', destination, [
-      'output 0',
-      'error 0',
-      'output 29',
-      'error 29',
-    ]
-  end
+  let(:args) { "'#{helper_scripts_path}/#{helper_script} #{print_sh_counter} #{print_sh_exit_code}' #{run_command_args}" }
 
-  context 'successful command run' do
-    let(:print_sh_exit_code) { SUCCESS_EXIT_CODE }
+  describe 'test standard failing filter' do
+    let(:helper_script) { 'print.sh' }
 
-    it_behaves_like 'prints lines to', :log
-    it_behaves_like 'prints lines to', :stdout
+    shared_examples_for 'prints lines to' do |destination|
+      it_behaves_like 'should print to', destination, [
+        'output 0',
+        'error 0',
+        'output 29',
+        'error 29',
+      ]
+    end
 
-    context 'hide_output specified' do
-      let(:run_command_args) { "'Running command' hide_output" }
+    context 'successful command run' do
+      let(:print_sh_exit_code) { SUCCESS_EXIT_CODE }
 
       it_behaves_like 'prints lines to', :log
-      it_behaves_like 'should print to', :stdout, 'Running command . OK'
+      it_behaves_like 'prints lines to', :stdout
+
+      context 'hide_output specified' do
+        let(:run_command_args) { "'Running command' hide_output" }
+
+        it_behaves_like 'prints lines to', :log
+        it_behaves_like 'should print to', :stdout, 'Running command . OK'
+      end
     end
-  end
 
-  context 'failed command run' do
-    let(:print_sh_exit_code) { ERROR_EXIT_CODE }
+    context 'failed command run' do
+      let(:print_sh_exit_code) { ERROR_EXIT_CODE }
 
-    it_behaves_like 'prints lines to', :log
-    it_behaves_like 'prints lines to', :stdout
+      it_behaves_like 'prints lines to', :log
+      it_behaves_like 'prints lines to', :stdout
 
-    describe 'should print last 20 lines of command stderr/stdout' do
-      it_behaves_like 'should print to', :stderr, ['error 10', 'error 29', 'output 10', 'output 29']
-      it_behaves_like 'should not print to', :stderr, ['error 0', 'error 9', 'output 0', 'output 9']
+      describe 'should print last 20 lines of command stderr/stdout' do
+        it_behaves_like 'should print to', :stderr, ['error 10', 'error 29', 'output 10', 'output 29']
+        it_behaves_like 'should not print to', :stderr, ['error 0', 'error 9', 'output 0', 'output 9']
+      end
     end
   end
 end
