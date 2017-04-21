2a46addd1e9d29d5dccb3263c4d31f5de0e09d0f
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2017-04-21 17:42:33 +0700

    Keep special chars when emulating stdout/stderr in tests

--- scripts/spec/scripts/run_command_tester/cat_files.sh
+++ scripts/spec/scripts/run_command_tester/cat_files.sh
@@ -3,7 +3,11 @@ STDOUT_FILE="${1}"
 STDERR_FILE="${2}"
 EXIT_CODE=${3:-0}
 
-echo -e "\033[32m$(cat ${STDOUT_FILE})\033[0m"
-echo -e "\033[31m$(cat ${STDERR_FILE})\033[0m" >&2
+echo -e "\033[32m"
+cat "${STDOUT_FILE}"
+echo -e "\033[0m"
+echo -e "\033[31m" >&2
+cat "${STDERR_FILE}" >&2
+echo -e "\033[0m" >&2
 
 exit ${EXIT_CODE}
