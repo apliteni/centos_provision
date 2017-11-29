f5bc4b64ad0787b26f67c233f92ac1886b4f3a40
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2017-11-29 17:24:09 +0700

    Disable character filtration on user input

--- scripts/enable-ssl.sh
+++ scripts/enable-ssl.sh
@@ -104,6 +104,7 @@ DICT['en.no']='no'
 DICT['en.prompt_errors.validate_domains_list']='Please enter domains list, separated by comma without spaces (i.e. domain1.tld,www.domain1.tld). Each domain name must consist of only letters, numbers and hyphens and contain at least one dot.'
 DICT['en.prompt_errors.validate_presence']='Please enter value'
 DICT['en.prompt_errors.validate_yes_no']='Please answer "yes" or "no"'
+DICT['en.prompt_errors.validate_ip']='Please enter valid IPv4 address (ex. 8.8.8.8)'
 
 DICT['ru.errors.program_failed']='ОШИБКА ВЫПОЛНЕНИЯ ПРОГРАММЫ'
 DICT['ru.errors.must_be_root']='Эту программу может запускать только root.'
@@ -117,6 +118,7 @@ DICT['ru.no']='нет'
 DICT['ru.prompt_errors.validate_domains_list']='Укажите список доменных имён через запятую без пробелов (например domain1.tld,www.domain1.tld). Каждое доменное имя должно состоять только из букв, цифр и тире и содержать хотябы одну точку.'
 DICT['ru.prompt_errors.validate_presence']='Введите значение'
 DICT['ru.prompt_errors.validate_yes_no']='Ответьте "да" или "нет" (можно также ответить "yes" или "no")'
+DICT['ru.prompt_errors.validate_ip']='Введите корректный IPv4 адрес (например 8.8.8.8)'
 
 
 declare -a DOMAINS
@@ -376,7 +378,7 @@ read_stdin(){
   else
     read -r variable
   fi
-  echo "$variable" | sed 's/[^a-zA-Z[:digit:][:punct:]]//g'
+  echo "$variable"
 }
 
 
@@ -740,7 +742,7 @@ validate_presence(){
 is_no(){
   local answer="${1}"
   shopt -s nocasematch
-  [[ "$answer" =~ ^(no|n|нет|н) ]]
+  [[ "$answer" =~ ^(no|n|нет|н)$ ]]
 }
 
 
@@ -748,7 +750,7 @@ is_no(){
 is_yes(){
   local answer="${1}"
   shopt -s nocasematch
-  [[ "$answer" =~ ^(yes|y|да|д) ]]
+  [[ "$answer" =~ ^(yes|y|да|д)$ ]]
 }
 
 
--- scripts/test-run-command.sh
+++ scripts/test-run-command.sh
@@ -105,6 +105,7 @@ DICT['en.no']='no'
 DICT['en.prompt_errors.validate_domains_list']='Please enter domains list, separated by comma without spaces (i.e. domain1.tld,www.domain1.tld). Each domain name must consist of only letters, numbers and hyphens and contain at least one dot.'
 DICT['en.prompt_errors.validate_presence']='Please enter value'
 DICT['en.prompt_errors.validate_yes_no']='Please answer "yes" or "no"'
+DICT['en.prompt_errors.validate_ip']='Please enter valid IPv4 address (ex. 8.8.8.8)'
 
 DICT['ru.errors.program_failed']='ОШИБКА ВЫПОЛНЕНИЯ ПРОГРАММЫ'
 DICT['ru.errors.must_be_root']='Эту программу может запускать только root.'
@@ -118,6 +119,7 @@ DICT['ru.no']='нет'
 DICT['ru.prompt_errors.validate_domains_list']='Укажите список доменных имён через запятую без пробелов (например domain1.tld,www.domain1.tld). Каждое доменное имя должно состоять только из букв, цифр и тире и содержать хотябы одну точку.'
 DICT['ru.prompt_errors.validate_presence']='Введите значение'
 DICT['ru.prompt_errors.validate_yes_no']='Ответьте "да" или "нет" (можно также ответить "yes" или "no")'
+DICT['ru.prompt_errors.validate_ip']='Введите корректный IPv4 адрес (например 8.8.8.8)'
 
 
 
@@ -184,7 +186,7 @@ read_stdin(){
   else
     read -r variable
   fi
-  echo "$variable" | sed 's/[^a-zA-Z[:digit:][:punct:]]//g'
+  echo "$variable"
 }
 
 
