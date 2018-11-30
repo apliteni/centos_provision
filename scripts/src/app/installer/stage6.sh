038cc703cb351bafb2cc2a626171a62399a47370
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2018-11-30 14:43:59 +0700

    Move stage5 to stage6

--- scripts/src/app/installer/stage6.sh
+++ scripts/src/app/installer/stage6.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env powscript
+#!/usr/bin/env bash
 
-
-stage5()
-  debug "Starting stage 5: run ansible playbook"
+stage6(){
+  debug "Starting stage 6: run ansible playbook"
   download_provision
   run_ansible_playbook
   run_ssl_enabler
   clean_up
   show_successful_message
   remove_log_files
+}
