f388641e04912c2419555aabf4a4f729817c02b7
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2019-01-31 19:32:03 +0700

    Regenerate nginx config in add-site tool by the new way

--- scripts/src/app/site_adder/stage4.sh
+++ scripts/src/app/site_adder/stage4.sh
@@ -3,7 +3,9 @@
 stage4(){
   debug "Starting stage 4: add vhost"
   ensure_can_add_vhost
-  add_vhost
+  for domain in ${VARS['site_domains']//,/ }; do
+    add_vhost $domain
+  done
   reload_nginx
   show_successful_message
 }
--- scripts/src/app/site_adder/stage4/ensure_can_add_vhost.sh
+++ scripts/src/app/site_adder/stage4/ensure_can_add_vhost.sh
@@ -3,11 +3,9 @@
 ensure_can_add_vhost(){
   debug "Ensure can add vhost"
   if is_path_exist "$(vhost_filepath)" "no"; then
-    local message="$(translate 'errors.vhost_already_exists')"
-    fail "${message/:vhost_filepath:/$(vhost_filepath)}"
+    fail "$(translate 'errors.vhost_already_exists' "vhost_filepath=$(vhost_filepath)")"
   fi
   if ! is_directory_exist "${VARS['site_root']}"; then
-    local message="$(translate 'errors.site_root_not_exists')"
-    fail "${message/:site_root:/${VARS['site_root']}}"
+    fail "$(translate 'errors.site_root_not_exists' "site_root=${VARS['site_root']})"
   fi
 }
