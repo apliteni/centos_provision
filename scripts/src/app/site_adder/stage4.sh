ea9def2c3f18c6218cf05b5350c08a5301937eb2
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2019-02-04 15:10:47 +0700

    Fix add-site.sh

--- scripts/src/app/site_adder/stage4.sh
+++ scripts/src/app/site_adder/stage4.sh
@@ -4,7 +4,7 @@ stage4(){
   debug "Starting stage 4: add vhost"
   ensure_can_add_vhost
   for domain in ${VARS['site_domains']//,/ }; do
-    add_vhost $domain
+    generate_nginx_host_config $domain
   done
   reload_nginx
   show_successful_message
--- scripts/src/app/site_adder/stage4/ensure_can_add_vhost.sh
+++ scripts/src/app/site_adder/stage4/ensure_can_add_vhost.sh
@@ -6,6 +6,6 @@ ensure_can_add_vhost(){
     fail "$(translate 'errors.vhost_already_exists' "vhost_filepath=$(vhost_filepath)")"
   fi
   if ! is_directory_exist "${VARS['site_root']}"; then
-    fail "$(translate 'errors.site_root_not_exists' "site_root=${VARS['site_root']})"
+    fail "$(translate 'errors.site_root_not_exists' "site_root=${VARS['site_root']}")"
   fi
 }
