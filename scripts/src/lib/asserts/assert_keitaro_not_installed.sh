4a4b100828517a5de65e6c84f724fab4ce1310af
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2019-02-06 23:02:00 +0700

    Do not check install.lock if reconfigure mode set

--- scripts/src/lib/asserts/assert_keitaro_not_installed.sh
+++ scripts/src/lib/asserts/assert_keitaro_not_installed.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
+#
+
+
+
+
 
 assert_keitaro_not_installed(){
   debug 'Ensure keitaro is not installed yet'
+  if isset "$RECONFIGURE"; then
+    debug 'Skip checking install.lock'
+    return
+  fi
   if is_exists_file /var/www/keitaro/var/install.lock no; then
     debug 'NOK: keitaro is already installed'
     fail "$(translate errors.keitaro_already_installed)"
