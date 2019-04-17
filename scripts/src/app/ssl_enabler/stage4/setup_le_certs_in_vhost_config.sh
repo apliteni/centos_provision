98c4968b1ef76768160c3a49cb373a5c3950db64
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2019-04-17 16:20:57 +0700

    Add post-processing marks to generated nginx configs

--- scripts/src/app/ssl_enabler/stage4/setup_le_certs_in_vhost_config.sh
+++ scripts/src/app/ssl_enabler/stage4/setup_le_certs_in_vhost_config.sh
@@ -4,6 +4,6 @@ setup_le_certs_in_vhost_config(){
   local domain="${1}"
   local certs_root_path="/etc/letsencrypt/live/${domain}"
   regenerate_vhost_config "$domain" 'messages.generating_nginx_config_for' \
-    "ssl_certificate ${certs_root_path}/fullchain.pem" \
-    "ssl_certificate_key ${certs_root_path}/privkey.pem"
+    "s|ssl_certificate .*|ssl_certificate ${certs_root_path}/fullchain.pem;|" \
+    "s|ssl_certificate_key .*|ssl_certificate_key ${certs_root_path}/privkey.pem;|"
   }
