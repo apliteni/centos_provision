b487f49ff9f375a65850824a88264142decffb68
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2019-04-17 10:02:55 +0700

    Fix add-site tool

--- scripts/add-site.sh
+++ scripts/add-site.sh
@@ -595,10 +595,12 @@ get_vhost_override_path(){
   echo "${NGINX_VHOSTS_DIR}/local/${domain}.inc"
 }
 
+
 get_vhost_path(){
   local domain="${1}"
   echo "${NGINX_VHOSTS_DIR}/${domain}.conf"
 }
+
 
 get_vhost_backup_path(){
   local domain="${1}"
@@ -1193,59 +1195,6 @@ stage2(){
   assert_caller_root
   assert_installed 'nginx' 'errors.reinstall_keitaro'
   assert_server_configuration_relevant
-  assert_nginx_configured
-}
-
-
-#
-
-
-
-
-
-
-assert_nginx_configured(){
-  if ! is_nginx_properly_configured; then
-    fail "$(translate 'errors.reinstall_keitaro')" "see_logs"
-  fi
-}
-
-
-is_nginx_properly_configured(){
-  if ! is_file_exist "${NGINX_KEITARO_CONF}"; then
-    log_and_print_err "ERROR: File ${NGINX_KEITARO_CONF} doesn't exists"
-    return ${FAILURE_RESULT}
-  fi
-  if ! is_directory_exist "${WEBROOT_PATH}"; then
-    log_and_print_err "ERROR: Directory ${WEBROOT_PATH} doesn't exists"
-    return ${FAILURE_RESULT}
-  fi
-  is_keitaro_configured
-}
-
-
-is_keitaro_configured(){
-  debug "Checking keitaro params in ${NGINX_KEITARO_CONF}"
-  if isset "$SKIP_CHECKS"; then
-    debug "SKIP: аctual check of keitaro params in ${NGINX_KEITARO_CONF} disabled"
-    FASTCGI_PASS_LINE="fastcgi_pass unix:/var/run/php70-fpm.sock;"
-    return ${SUCCESS_RESULT}
-  fi
-  if grep -q -e "root ${WEBROOT_PATH};" "${NGINX_KEITARO_CONF}"; then
-    FASTCGI_PASS_LINE="$(cat "$NGINX_KEITARO_CONF" | grep fastcgi_pass | sed 's/^ +//' | head -n1)"
-    if empty "${FASTCGI_PASS_LINE}"; then
-      log_and_print_err "ERROR: ${NGINX_KEITARO_CONF} is not properly configured (can't find 'fastcgi_pass ...;' directive)"
-      log_and_print_err "$(print_content_of ${NGINX_KEITARO_CONF})"
-      return ${FAILURE_RESULT}
-    else
-      debug "OK: it seems like ${NGINX_KEITARO_CONF} is properly configured"
-      return ${SUCCESS_RESULT}
-    fi
-  else
-    log_and_print_err "ERROR: ${NGINX_KEITARO_CONF} is not properly configured (can't find 'root ${WEBROOT_PATH};' directive"
-    log_and_print_err $(print_content_of ${NGINX_KEITARO_CONF})
-    return ${FAILURE_RESULT}
-  fi
 }
 
 
