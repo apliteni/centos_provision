914db8209fe9c157a8e8197de3311991124db2f0
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2019-04-24 12:09:12 +0700

    Allow to issue SSL for sites generated by add-site

--- scripts/src/app/site_adder/stage4/generate_vhost_site_adder.sh
+++ scripts/src/app/site_adder/stage4/generate_vhost_site_adder.sh
@@ -2,7 +2,6 @@
 
 generate_vhost_site_adder(){
   local domain="${1}"
-  debug "Add vhost"
   generate_vhost "$domain" 'messages.add_vhost' \
                           "s|root .*|root ${VARS['site_root']};|" \
                           "/locations-tracker.inc/d"
