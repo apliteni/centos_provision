6c65f886c70c8c685b222a7389073e0ab1280f1b
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2019-04-24 20:11:06 +0700

    Support non-interactive mode for add-site

--- scripts/src/app/site_adder/stage3/get_user_vars.sh
+++ scripts/src/app/site_adder/stage3/get_user_vars.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
+#
+
+
+
+
 
 get_user_vars(){
   debug 'Read vars from user input'
   hack_stdin_if_pipe_mode
-  get_user_var 'site_domains' 'validate_presence validate_domains_list'
-  VARS['site_root']="/var/www/$(first_domain)"
-  get_user_var 'site_root' 'validate_presence'
+  if empty "${VARS['site_root']}"; then
+    get_user_var 'site_domains' 'validate_presence validate_domains_list'
+  fi
+  if empty "${VARS['site_root']}"; then
+    VARS['site_root']="/var/www/$(first_domain)"
+    get_user_var 'site_root' 'validate_presence'
+  fi
 }
