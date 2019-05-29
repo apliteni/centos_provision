32372840e0b471504ed4276aa02e703ef51b1535
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2019-05-29 10:44:23 +0700

    Set roadrunner as default php engine

--- scripts/src/app/installer/vars/common.sh
+++ scripts/src/app/installer/vars/common.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 PROVISION_DIRECTORY="centos_provision-${RELEASE_BRANCH}"
 KEITARO_ALREADY_INSTALLED_RESULT=2
-PHP_ENGINE=php-fpm
+PHP_ENGINE=roadrunner
 DETECTED_PREFIX_PATH=".keitaro_detected_prefix"
 TABLES_PREFIX=""
