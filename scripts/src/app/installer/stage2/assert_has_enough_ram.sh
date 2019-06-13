fae72f9071aaa1ed80aead6966821d16a1216130
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2019-06-13 14:08:49 +0700

    Fix memory size checking condition

--- scripts/src/app/installer/stage2/assert_has_enough_ram.sh
+++ scripts/src/app/installer/stage2/assert_has_enough_ram.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+MIN_SIZE_KB=2000000
 
 assert_has_enough_ram(){
+  debug "Checking RAM size"
   if is_file_exist /proc/meminfo no; then
-    memsize_kb=$(cat /proc/meminfo | head -n1 | awk '{print $2}')
-    if (( memsize_kb -lt 2000000 )); then
+    local memsize_kb=$(cat /proc/meminfo | head -n1 | awk '{print $2}')
+    if [[ "$memsize_kb" -lt "$MIN_SIZE_KB" ]]; then
+      debug "RAM size ${current_size_kb}kb is less than ${MIN_SIZE_KB}, raising error"
       fail "$(translate errors.not_enough_ram)"
+    else
+      debug "RAM size ${current_size_kb}kb is greater than ${MIN_SIZE_KB}, continuing"
     fi
   fi
 }
