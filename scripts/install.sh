ec19a6db8071eb2319b2346e7225b3d484686681
Dmitry Lihachev <dmitry.a.lihachev@gmail.com>
2019-07-01 17:28:29 +0700

    Add ability to set PHP_ENGINE via env variable

--- scripts/install.sh
+++ scripts/install.sh
@@ -1242,7 +1242,7 @@ validate_yes_no(){
 
 PROVISION_DIRECTORY="centos_provision-${RELEASE_BRANCH}"
 KEITARO_ALREADY_INSTALLED_RESULT=2
-PHP_ENGINE=roadrunner
+PHP_ENGINE=${PHP_ENGINE:-roadrunner}
 DETECTED_PREFIX_PATH=".keitaro_detected_prefix"
 
 
