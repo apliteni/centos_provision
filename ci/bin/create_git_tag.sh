cd1ea55b1edd87bcc69829cde5e7ab2d2bec35a1
Qj7 <vladimir.dolgiy@apliteni.com>
2019-10-17 03:32:55 +0300

    test with new version without f

--- ci/bin/create_git_tag.sh
+++ ci/bin/create_git_tag.sh
@@ -5,4 +5,4 @@ RELEASE_VERSION="$(cat $ROOT_DIR/RELEASE_VERSION)"
 
 set -e -x -o pipefail
 # add || true to end of the curl command for debuging that always return success in job
-curl -X POST --show-error --fail "https://gitlab.x.apli.tech/api/v4/projects/${CI_PROJECT_ID}/repository/tags?tag_name=v${RELEASE_VERSION}&ref=${CI_COMMIT_SHA}&private_token=${GITLAB_TOKEN}"
+curl -X POST --show-error --fail "https://gitlab.x.apli.tech/api/v4/projects/${CI_PROJECT_ID}/repository/tags?tag_name=v${RELEASE_VERSION}&ref=${CI_COMMIT_SHA}&private_token=${GITLAB_TOKEN}" || true
