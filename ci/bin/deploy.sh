de0b1f716dab3056ee7ebf9b2994817af0555d0f
Artur Sabirov <artur.sabirov@apliteni.com>
2019-10-21 10:25:50 +0200

    INST-40 fix ci

--- ci/bin/deploy.sh
+++ ci/bin/deploy.sh
@@ -4,5 +4,7 @@ RELEASE_VERSION="$(cat RELEASE_VERSION)"
 
 git commit -a -m "Inject scripts v${RELEASE_VERSION}"
 git push -f -u ${GIT_URL} `git rev-parse HEAD`:refs/heads/${CI_COMMIT_REF_NAME}
+git tag -a v${RELEASE_VERSION} -m "v${RELEASE_VERSION}"
+git push -u ${GIT_URL} --tags
 git push -f -u ${GIT_URL} `git rev-parse HEAD`:refs/heads/release-${RELEASE_VERSION}
 git push -f -u ${GIT_URL} `git rev-parse HEAD`:refs/heads/current
