14c1befeb62f0a27bc3d6e4a98f4180e9a509f77
Qj7 <vladimir.dolgiy@apliteni.com>
2019-10-22 02:47:41 +0300

    INST-8 add script

---
+++ scripts/src/app/installer/stage1/check_thp_disable_possibility.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+check_thp(){
+  echo never > /sys/kernel/mm/transparent_hugepage/enabled && echo never > /sys/kernel/mm/transparent_hugepage/defrag
+  ERROR='\033[0;31m'
+  NC='\033[0m' # put to end of string for remove color scheme after error
+  thp_defrag="$(cat /sys/kernel/mm/transparent_hugepage/defrag)"
+  thp_enabled="$(cat /sys/kernel/mm/transparent_hugepage/enabled)"
+  if [ "$thp_defrag" == "$thp_enabled" ]; then
+    if [ "$thp_enabled" == "always madvise [never]" ]; then
+      echo "thp disabled... OK"
+    else
+      echo -e "${ERROR}Невозможно отключить thp. Установка будет прервана${NC}"
+      exit 1
+    fi
+  else
+    echo -e "${ERROR}Невозможно отключить thp. Установка будет прервана${NC}"
+  fi
+}
