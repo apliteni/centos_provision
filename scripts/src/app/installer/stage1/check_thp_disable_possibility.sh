803563f7772c47f0ff13165f73a9ca682d4e738d
Qj7 <vladimir.dolgiy@apliteni.com>
2019-10-28 21:44:15 +0200

    INST-8 test with msg methods

--- scripts/src/app/installer/stage1/check_thp_disable_possibility.sh
+++ scripts/src/app/installer/stage1/check_thp_disable_possibility.sh
@@ -4,22 +4,23 @@
 
 check_thp_disable_possibility(){
   if pgrep "/sys/kernel/mm/transparent_hugepage/enabled" &>/dev/null; then
-    echo 'thp отсутствует в системе'
+    print_with_color "thp not allowed in this system" 'grey'
   else
     echo never > /sys/kernel/mm/transparent_hugepage/enabled && echo never > /sys/kernel/mm/transparent_hugepage/defrag
-    error_message="\e[91mНевозможно отключить thp. Установка будет прервана\033[0m"
     thp_defrag="$(cat /sys/kernel/mm/transparent_hugepage/defrag)"
     thp_enabled="$(cat /sys/kernel/mm/transparent_hugepage/enabled)"
     if [ "$thp_defrag" == "$thp_enabled" ]; then
       if [ "$thp_enabled" == "always madvise [never]" ]; then
-        echo -e "\e[32mthp disabled, thats OK\033[0m"
+        print_with_color "thp disabled" 'green'
       else
-        echo -e "${error_message}"
+        print_err "Impossible to disable thp install will be interrupted" 'red'
+        clean_up
         exit 1
       fi
     else
-      echo -e "${error_message}"
-      exit 1
+        print_err "Impossible to disable thp install will be interrupted" 'red'
+        clean_up
+        exit 1
     fi
   fi
 }
