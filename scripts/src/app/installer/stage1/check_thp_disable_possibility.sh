5706d487a7818adee6af5d44f4c4310d123d0663
Vladimir Dolgiy <vladimir.dolgiy@apliteni.com>
2019-11-21 17:02:03 +0200

    INST-75 fix

--- scripts/src/app/installer/stage1/check_thp_disable_possibility.sh
+++ scripts/src/app/installer/stage1/check_thp_disable_possibility.sh
@@ -11,15 +11,13 @@ check_openvz(){
 }
 
 check_thp_disable_possibility(){
-  if isset "${CI}"; then
-    if pgrep "/sys/kernel/mm/transparent_hugepage/enabled" &>/dev/null; then
-      print_with_color "thp not allowed in this system" 'grey'
-    else
+  if empty "${CI}"; then
+    if is_file_exist "/sys/kernel/mm/transparent_hugepage/enabled" && is_file_exist "/sys/kernel/mm/transparent_hugepage/defrag"; then
       echo never > /sys/kernel/mm/transparent_hugepage/enabled && echo never > /sys/kernel/mm/transparent_hugepage/defrag
-      thp_defrag="$(cat /sys/kernel/mm/transparent_hugepage/defrag)"
       thp_enabled="$(cat /sys/kernel/mm/transparent_hugepage/enabled)"
       if isset "$thp_enabled" && "$thp_enabled" == "always madvise [never]"; then
-        print_with_color "thp disabled" 'green'
+        print_with_color "Before installation check possibility to disalbe THP" 'blue'
+        print_with_color ". OK" 'green'
       else
         print_err "Impossible to disable thp install will be interrupted" 'red'
         clean_up
