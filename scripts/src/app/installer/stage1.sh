f59477ff53094b8ca04f66c85caeb6861ce8aa99
Vladimir Dolgiy <vladimir.dolgiy@apliteni.com>
2019-11-21 06:03:25 +0200

    INST-75 check openvz some refactoring fix ci

--- scripts/src/app/installer/stage1.sh
+++ scripts/src/app/installer/stage1.sh
@@ -2,7 +2,9 @@
 
 stage1(){
   debug "Starting stage 1: initial script setup"
-  check_thp_disable_possibility
+  if isset "${CI}"; then
+    check_thp_disable_possibility
+  fi
   parse_options "$@"
   set_ui_lang
 }
