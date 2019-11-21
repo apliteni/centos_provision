2e10e5a1bd484467d3b4d3897484743ab5975ea0
Vladimir Dolgiy <vladimir.dolgiy@apliteni.com>
2019-11-21 18:34:37 +0200

    INST-75 fix

--- scripts/src/app/installer/stage1/check_thp_disable_possibility.sh
+++ scripts/src/app/installer/stage1/check_thp_disable_possibility.sh
@@ -16,8 +16,7 @@ check_thp_disable_possibility(){
       echo never > /sys/kernel/mm/transparent_hugepage/enabled && echo never > /sys/kernel/mm/transparent_hugepage/defrag
       thp_enabled="$(cat /sys/kernel/mm/transparent_hugepage/enabled)"
       if [ "$thp_enabled" == "always madvise [never]" ]; then
-        print_with_color "Before installation check possibility to disalbe THP" 'blue'
-        echo -ne "\e[${COLOR_CODE['green']}m. OK ${RESET_FORMATTING}"
+        echo -e "\e[${COLOR_CODE['blue']}mBefore installation check possibility to disalbe THP \e[${COLOR_CODE['green']}m. OK ${RESET_FORMATTING}"
       else
         print_err "Impossible to disable thp install will be interrupted" 'red'
         clean_up
